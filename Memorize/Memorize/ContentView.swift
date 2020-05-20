a892621e2d6141b358581f2eb05dc1fff561b4e1
Vlad <vlad.j.work@gmail.com>
2020-05-19 23:08:51 -0400

    init

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Vladislav Jidkov on 2020-05-19.
+//  Copyright © 2020 Vladislav Jidkov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
